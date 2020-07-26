89a3cef88c1a9304a91581a9cc835557530c1942
Jonathan Min <39451363+min-j@users.noreply.github.com>
2020-07-25 21:29:07 -0400

    used matplotlib to plot data points the ease of use with pyplot removes the need for user inputs and provides a good visualization that, I believe, a Seaborn heatmap could not do

--- calc.py
+++ calc.py
@@ -1,11 +1,11 @@
 import matplotlib.pyplot as plt
-from matplotlib import style
+# import numpy as np
 import pandas as pd
 import PySimpleGUI as sg
+from pandas.plotting import register_matplotlib_converters
 
 # --Initialization --
 df = pd.read_csv('data/boroughs-case-hosp-death.csv', parse_dates=True, index_col=0)
-# print(df.tail(10))
 
 boros = {
     'BROOKLYN': "BK",
@@ -27,6 +27,12 @@ def getData(date, boro, count):
     return df[col][date]  # df col row
 
 
+def getTotal(date, count):
+    return getData(date, "MANHATTAN", count) + getData(date, "BROOKLYN", count) + \
+           getData(date, "QUEENS", count) + getData(date, "BRONX", count) + \
+           getData(date, "STATEN ISLAND", count)
+
+
 def dodPercentChange():
     # today = main()
     date1 = input("Choose the first (earlier) date (in YYYY-MM-DD format): ")
@@ -41,12 +47,31 @@ def infectionRate(num):
     return num / nycPop * 100
 
 
+def showGraph(boro, count):
+    register_matplotlib_converters()
+    dates = pd.date_range(start="2020-02-29", end="2020-07-07")
+    countList = []
+    for i in dates:
+        if boro == "ALL":
+            countList.append(getTotal(i, count))
+        else:
+            countList.append(getData(i, boro, count))
+    plt.plot(dates, countList)
+    plt.ylabel('COUNT')
+    plt.xlabel('DATE')
+    plt.xticks(rotation=30)
+    plt.title(boro + " " + count)
+    plt.show()
+
+
 def main():
+    choice = ""
     print("Welcome to the COVID-19 Calculator")
     print("What would you like to do?")
     print("1. Look at data for a specific day.")
     print("2. Calculate percent change between two dates.")
     print("3. Calculate the infection rate for a specific day.")
+    print("4. Plot a graph.")
     while True:
         try:
             choice = int(input("Enter a number as your choice. \n"))
@@ -68,13 +93,18 @@ def main():
         print(dodPercentChange())
     elif choice == 3:
         date = input("\n" + "Choose a date (in YYYY-MM-DD). \n")
-        cases = getData(date, "MANHATTAN", "CASE COUNT") + getData(date, "BROOKLYN", "CASE COUNT") + \
-                getData(date, "QUEENS", "CASE COUNT") + getData(date, "BRONX", "CASE COUNT") + \
-                getData(date, "STATEN ISLAND", "CASE COUNT")
+        cases = getTotal(date, "CASE COUNT")
         rate = infectionRate(cases)
         print(date + ": " + "TOTAL CASES: " + str(cases) + " Infection Rate: " + str(round(rate, 5)) + "%.")
+    elif choice == 4:
+        boro = input("What borough do you want to see? Type all for total data. \n").upper()
+        count = input("What type of data? Enter case count, hospitalized count, or death count \n").upper()
+        showGraph(boro, count)
+        print("DISPLAYING GRAPH")
 
 
+# testing PySimpleGui
+# drop downs exist
 def run():
     # sg.theme('Dark Blue 3')  # please make your creations colorful
     one = "Look at data for a specific day"
@@ -83,7 +113,8 @@ def run():
     layout = [[sg.Text('Welcome to the Coronavirus Calculator')],
               [sg.Button(one)],
               [sg.Button(two)],
-              [sg.Button(three)]]
+              [sg.Button(three)],
+              ]
     window = sg.Window('Coronavirus Calculator', layout)
     while True:
         event, values = window.read()
@@ -92,4 +123,6 @@ def run():
     window.close()
 
 
+# Running Methods
 main()
+# showGraph("CASE COUNT")
